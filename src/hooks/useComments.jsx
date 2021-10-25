c8b5c3342b73b26f5dd5084042e1d07320923ce8
jatebo <john.tenbosch@gmail.com>
2021-10-25 09:58:54 +0100

    add boolean check to useEffect to avoid memory leak

--- src/hooks/useComments.jsx
+++ src/hooks/useComments.jsx
@@ -7,18 +7,24 @@ const useComments = (review_id) => {
   const [page, setPage] = useState(1);
 
   useEffect(() => {
+    let isMounted = true;
     setIsLoading(true);
     api
       .fetchComments(review_id, page)
       .then((reviewData) => {
-        setIsLoading(false);
-        setComments(reviewData);
-        if (reviewData.length === 0 && page > 1)
-          setPage((currPage) => currPage - 1);
+        if (isMounted) {
+          setIsLoading(false);
+          setComments(reviewData);
+          if (reviewData.length === 0 && page > 1)
+            setPage((currPage) => currPage - 1);
+        }
       })
       .catch((err) => {
         console.dir(err);
       });
+    return () => {
+      isMounted = false;
+    };
   }, [review_id, page]);
 
   return { comments, setComments, isLoading, setPage, page };
